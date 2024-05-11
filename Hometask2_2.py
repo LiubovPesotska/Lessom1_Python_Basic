b9ef50704a27ebda692d673bc9b5be78a24a5e92
LiubovPesotska <prudkova.pr@gmail.com>
2024-05-11 20:54:19 +0300

    Added all new things

---
+++ Hometask2_2.py
@@ -0,0 +1,8 @@
+number=int(input("Enter a number: "))
+n1=number//10000
+n2=number//1000%10
+n3=number//100%10
+n4=number//10%10
+n5=number%10
+result=n5*10000+n4*1000+n3*100+n2*10+n1
+print ("Result",result)
