daaed17e6690fdcd1bdf226064237bbb28c4703b
LiubovPesotska <prudkova.pr@gmail.com>
2024-05-11 17:11:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print ("Hello World Liubov")
+print ()
+print ("test")
+print ("Hello World", "Text1", "Text2", sep= ",", end="-")
+print ("Next step")
+# а тут мій коментар
+print("Hello\nWorld")
+print ("This is again me \n \t Liubov")
+print ("The name of the brand is \"Lvivska Kava\"")
+print ("To check what we had added")
