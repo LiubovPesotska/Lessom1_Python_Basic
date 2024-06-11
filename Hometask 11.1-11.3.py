b043e59d51646795b2697a7737b02da959b36829
LiubovPesotska <prudkova.pr@gmail.com>
2024-06-11 18:11:30 +0300

    Added all new things

---
+++ Hometask 11.1-11.3.py
@@ -0,0 +1,60 @@
+#Hometask 11.1
+def prime_generator(end):
+    def is_prime(n):
+        if n <= 1:
+            return False
+        if n <= 3:
+            return True
+        if n % 2 == 0 or n % 3 == 0:
+            return False
+        i = 5
+        while i * i <= n:
+            if n % i == 0 or n % (i + 2) == 0:
+                return False
+            i += 6
+        return True
+
+    number = 2
+    while number <= end:
+        if is_prime(number):
+            yield number
+        number += 1
+
+# Testing the generator
+from inspect import isgenerator
+
+gen = prime_generator(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(prime_generator(10)) == [2, 3, 5, 7], 'Test1'
+assert list(prime_generator(15)) == [2, 3, 5, 7, 11, 13], 'Test2'
+assert list(prime_generator(29)) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29], 'Test3'
+print('Ok')
+
+#Hometask 11.2
+
+def generate_cube_numbers(end):
+    number = 2
+    while True:
+        cube = number ** 3
+        if cube >= end:
+            return
+        yield cube
+        number += 1
+
+from inspect import isgenerator
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729], '10 у кубі це 1000'
+print('Ok')
+
+#Hometask 11.3
+def is_even(number):
+    return (number & 1) == 0
+assert is_even(2494563894038**2) == True, 'Test1'
+assert is_even(1056897**2) == False, 'Test2'
+assert is_even(24945638940387**3) == False, 'Test3'
+
+print('All tests passed.')
